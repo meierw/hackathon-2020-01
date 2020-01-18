790576e5680821eaf2fdd026d6c28ff8d6aeb89f
Walter Meiren <valters.meirens@gmail.com>
2020-01-18 19:55:22 +0900

    Add image pre-processing logic

--- 01-rotation_correction/helper.py
+++ 01-rotation_correction/helper.py
@@ -1,11 +1,33 @@
+'''Helper methods for parsing images to feed into ML models'''
+import cv2
 import numpy as np
 
-def pad2DArrayWithZeros(numpyArray):
-    shape = numpyArray.shape
+def convert_to_square(image, size):
+    '''Resize an image array, and convert it to a square, by padding it with zeros'''
+    shape = image.shape
+    shorter_dim = get_shorter_dim(shape)
+    longer_dim = int(not shorter_dim)
+    if shorter_dim == -1:
+        return cv2.resize(image, (size, size))
+    dsize = [0, 0]
+    dsize[shorter_dim] = size
+    dsize[longer_dim] = int(size * (shape[shorter_dim] / shape[longer_dim]))
+    result = cv2.resize(image, tuple(dsize))
+    return pad_with_zeros(result)
+
+def pad_with_zeros(image):
+    '''Pad an an image array with zeros to give it a square shape'''
+    shape = image.shape
+    shorter_dim = get_shorter_dim(shape)
+    if shorter_dim == -1:
+        return image
     diff = shape[0] - shape[1]
-    if diff == 0:
-        return numpyArray
-    dimToPad = 1 if diff > 0 else 0
     padding = [[0, 0], [0, 0]]
-    padding[dimToPad][1] = abs(diff)
-    return np.pad(numpyArray, padding, mode='constant')
+    padding[shorter_dim][1] = abs(diff)
+    return np.pad(image, padding, mode='constant')
+
+def get_shorter_dim(shape):
+    '''Find the shortest dimension from a given 2D shape'''
+    if shape[0] == shape[1]:
+        return -1
+    return int(shape[1] < shape[0])
--- 01-rotation_correction/train.py
+++ 01-rotation_correction/train.py
@@ -1,12 +1,12 @@
+import os
+import cv2
 import numpy as np
 import helper
 
-# d0dcbcd6cd9243a38882210c6355078f.jpg
-
 # TODO:
-#   - Convert image to a square
+#   - Convert image to a square (done)
 #       - Add whitespace (zeros) instead of squishing, to give the model hints about which is the long side
-#   - Resize image to some fixed square value
+#   - Resize image to some fixed square value (done)
 #   - Train to identify pieces of paper from open books
 #       - Don't do anything with open books, yet. Those will need additional processing
 #   - Train to identify rotation of found pieces of paper
@@ -14,20 +14,12 @@ import helper
 # TODO (maybe):
 #   - Concatenate all four rotations of image into one square image
 
-a = np.array([
-    [1,2,3],
-    [4,5,6],
-])
-b = np.array([
-    [1,2],
-    [3,4],
-    [5,6]
-])
-c = np.array([
-    [1,2],
-    [3,4]
-])
+train_dir = os.path.dirname(__file__) + '/data/train'
+# img = cv2.imread(f'{train_dir}/d0dcbcd6cd9243a38882210c6355078f.jpg', cv2.IMREAD_GRAYSCALE)
+img = cv2.imread(f'{train_dir}/d3a855c97948435b9f3d2d140a941e66.jpg', cv2.IMREAD_GRAYSCALE)
+img = helper.convert_to_square(img, 1000)
+img = cv2.bitwise_not(img)
+img = img / 255
 
-print(helper.pad2DArrayWithZeros(a))
-print(helper.pad2DArrayWithZeros(b))
-print(helper.pad2DArrayWithZeros(c))
+cv2.imshow('hello', img)
+cv2.waitKey()
